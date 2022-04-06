7c23212991a135bcd3e8ea4f6c44ef50a6fd0c84
Ishan Vyas <ishan@deepsource.io>
2022-04-06 17:17:41 +0530

    Use different channels for stdout & stderr
    Signed-off-by: Ishan Vyas <ishan@deepsource.io>

--- src/User.php
+++ src/User.php
@@ -6,14 +6,16 @@ use Monolog\Logger;
 
 class User
 {
-    public function add(Logger $logger): void
+    public function add(Logger $stderrLogger, Logger $stdoutLogger): void
     {
-        $logger->info('This is information.', ['data' => ['name' => 'foo bar']]);
+        $stderrLogger->info('This is information.', ['data' => ['name' => 'foo bar']]);
+        $stderrLogger->debug('This is debug message.');
+        $stderrLogger->warning('This is warning');
+        $stderrLogger->error('This is errorrrr....');
 
-        $logger->debug('This is debug message.');
-
-        $logger->warning('This is warning');
-
-        $logger->error('This is errorrrr....');
+        $stdoutLogger->info('This is information.', ['data' => ['name' => 'stdin']]);
+        $stdoutLogger->debug('This is debug message.');
+        $stdoutLogger->warning('This is warning');
+        $stdoutLogger->error('This is errorrrr....');
     }
 }
